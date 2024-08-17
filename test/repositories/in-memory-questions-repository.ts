509dc0b98a60b62db718be108469e38c8d6e2e78
edumartinsg <edu19_96@hotmail.com>
2024-08-17 17:28:43 +1000

    aggregates & watched list

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,9 +1,14 @@
+import { PaginationParams } from "@/core/repositories/pagination-params"
+import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository"
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository"
 import { Question } from "@/domain/forum/enterprise/entities/question"
-import { PaginationParams } from "../../src/core/repositories/pagination-params"
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
+
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository
+  ) {}
 
   async findById(id: string) {
     const question = this.items.find((item) => item.id.toString() === id)
@@ -25,6 +30,14 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question
   }
 
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
+  }
+
   async create(question: Question) {
     this.items.push(question)
   }
@@ -39,13 +52,9 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
     this.items.splice(itemIndex, 1)
-  }
 
-  async findManyRecent({ page }: PaginationParams) {
-    const questions = this.items
-      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-      .slice((page - 1) * 20, page * 20)
-
-    return questions
+    this.questionAttachmentsRepository.deleteManyByQuestionId(
+      question.id.toString()
+    )
   }
 }
